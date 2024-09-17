c607e2852a992cae72ef0893a41ab88851c78b24
dev-xero <elo48858@gmail.com>
2024-09-17 14:48:17 +0100

    feat: Implement string literals

--- src/main/java/dev/xero/Scanner.java
+++ src/main/java/dev/xero/Scanner.java
@@ -31,6 +31,33 @@ class Scanner {
         tokens.add(new Token(type, text, literal, line));
     }
 
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string literal.");
+            return;
+        }
+
+        advance(); // The closing "
+        String value = source.substring(start + 1, current - 1); // Skip quotes
+        addToken(STRING, value);
+    }
+
     private void scanToken() {
         char ch = advance();
         switch (ch) {
@@ -44,6 +71,30 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break; // Ignore whitespace
+            case '"': string(); break;
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
