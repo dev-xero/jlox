2e141c28be341b962d1adcb1761a4dd8c40a33ff
dev-xero <elo48858@gmail.com>
2024-09-17 18:04:53 +0100

    feat: Implement identifier scanning

--- src/main/java/dev/xero/Scanner.java
+++ src/main/java/dev/xero/Scanner.java
@@ -1,7 +1,9 @@
 package dev.xero;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static dev.xero.TokenType.*;
 
@@ -11,6 +13,28 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     private boolean isAtEnd() {
         return current >= source.length();
@@ -53,6 +77,14 @@ class Scanner {
         return ch >= '0' && ch <= '9';
     }
 
+    private boolean isAlpha(char ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_';
+    }
+
+    private boolean isAlphaNumeric(char ch) {
+        return isAlpha(ch) || isDigit(ch);
+    }
+
     private void number() {
         while (isDigit(peek())) advance();
 
@@ -77,6 +109,16 @@ class Scanner {
         advance(); // The closing "
         String value = source.substring(start + 1, current - 1); // Skip quotes
         addToken(STRING, value);
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
     }
 
     private void scanToken() {
@@ -119,6 +161,8 @@ class Scanner {
             default:
                 if (isDigit(ch))
                     number();
+                else if (isAlpha(ch))
+                    identifier();
                 else
                     Lox.error(line, "Unexpected character.");
                 break;
