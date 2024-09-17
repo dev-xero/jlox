1756433aca4df80afadb919cd901467444887edc
dev-xero <elo48858@gmail.com>
2024-09-17 13:09:37 +0100

    feat: Initialize project

---
+++ src/main/java/dev/xero/Lox.java
@@ -0,0 +1,16 @@
+package dev.xero;
+
+import java.io.IOException;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+}
