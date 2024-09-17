b5dba2aadcb4985b4d6ac591e3e3e93bac7b9852
dev-xero <elo48858@gmail.com>
2024-09-17 13:11:44 +0100

    refactor: Define run file private method

--- src/main/java/dev/xero/Lox.java
+++ src/main/java/dev/xero/Lox.java
@@ -1,8 +1,16 @@
 package dev.xero;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Lox {
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
