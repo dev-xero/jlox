44a15c0ca3dcc0412b96eb0927db9708e212ca9a
dev-xero <elo48858@gmail.com>
2024-09-17 13:54:08 +0100

    feat: Include other single char tokens

--- src/main/java/dev/xero/Scanner.java
+++ src/main/java/dev/xero/Scanner.java
@@ -35,6 +35,15 @@ class Scanner {
         char ch = advance();
         switch (ch) {
             case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
         }
     }
 
